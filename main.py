bdd815d28ce5db66432a75c6985e48ba595c8479
yuchaoshen_hw01 <shen748@purdue.edu>
2025-12-05 12:38:22 -0500

    out put every liberty for black/white

--- main.py
+++ main.py
@@ -11,16 +11,30 @@ def main():
         return
 
     # Find max liberty
-    max_liberty = 0
+    # Find max liberty for Black and White
+    max_liberty_B = 0
+    max_liberty_W = 0
+    
     for y in range(game.size):
         for x in range(game.size):
             if game.board[y][x] != '*':
                 liberty = get_liberty(game, x, y)
-                if liberty > max_liberty:
-                    max_liberty = liberty
+                if game.board[y][x] == 'B':
+                    if liberty > max_liberty_B:
+                        max_liberty_B = liberty
+                elif game.board[y][x] == 'W':
+                    if liberty > max_liberty_W:
+                        max_liberty_W = liberty
     
-    # Print max liberty
-    print(f"The biggist liberty in this broad is {max_liberty}")
+    print(f"Max liberty for Black: {max_liberty_B}")
+    print(f"Max liberty for White: {max_liberty_W}")
+    
+    if max_liberty_B > max_liberty_W:
+        print("Black is more likely to win")
+    elif max_liberty_W > max_liberty_B:
+        print("White is more likely to win")
+    else:
+        print("Both are equally likely to win")
     
     return 0
 
